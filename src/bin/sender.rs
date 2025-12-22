65fba6a018c5c786a4eafc00cadd74464216f7cb
likanug <likanug.g@qq.com>
2025-12-22 13:30:49 +0800

    refactor: delete main.rs, split it to cli.rs and bin/sender.rs

---
+++ src/bin/sender.rs
@@ -0,0 +1,18 @@
+use sendmer::cli;
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let res = cli::run().await;
+
+    if let Err(e) = &res {
+        eprintln!("{e}");
+    }
+
+    match res {
+        Ok(()) => std::process::exit(0),
+        Err(_) => std::process::exit(1),
+    }
+}
