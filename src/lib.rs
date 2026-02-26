57054932cc01a877cb3979bf21c965406996dc0e
liukang <likanug.g@qq.com>
2026-02-26 13:44:59 +0800

    docs: update description for clarity on functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![warn(clippy::all)]
 #![warn(clippy::nursery)]
 
-//! sendmer: small CLI for sharing/downloading blob data
+//! sendmer: small CLI for send/receive blob data
 //!
 //! 这个 crate 暴露了库级 API（`send` 和 `receive`）给程序调用，
 //! 同时也包含用于命令行工具的包装（`src/bin`）。
